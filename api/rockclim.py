19a8030960df84d3bf83686a58384047eb8d0c38
Roger Lew <rogerlew@gmail.com>
2024-11-09 19:42:22 -0800

    Update rockclim.py

--- api/rockclim.py
+++ api/rockclim.py
@@ -11,6 +11,12 @@ from wepppy2.climates.cligen import CligenStationsManager, Cligen
 
 router = APIRouter()
 
+
+class Location(BaseModel):
+    longitude: float
+    latitude: float
+
+
 class ClimatePars(BaseModel):
     """
     ClimatePars is a Pydantic model representing the parameters for climate data.
@@ -33,14 +39,17 @@ class ClimatePars(BaseModel):
     par_id: Optional[str] = None
     input_years: Optional[int] = 100
     cligen_version: Optional[str] = "5.3.2"
+    location: Optional[Location] = None
+    use_prism: Optional[bool] = None 
     
     def __hash__(self):
         return hash((self.database, self.state_code, self.par_id, self.input_years, self.cligen_version))
     
+    
 @router.post("/rockclim/GET/stations_in_state")
 def stations_in_state(climate_pars: ClimatePars):
     stationManager = CligenStationsManager(climate_pars.database)
-    
+
     if climate_pars.state is None:
         return {"error": "State is required"}
     
@@ -48,19 +57,43 @@ def stations_in_state(climate_pars: ClimatePars):
 
     return [s.as_dict() for s in stations]
 
-@router.post("/rockclim/GET/station_par")
+
+@router.post("/rockclim/GET/get_closest_stations")
+def get_closest_stations(climate_pars: ClimatePars):
+    if climate_pars.location is None:
+        return  {"error": "Location is required"}
+    
+    stationManager = CligenStationsManager(climate_pars.database)
+    stations = stationManager.get_closest_stations(
+        (climate_pars.location.longitude, climate_pars.location.latitude), 
+        num_stations=10)
+
+    return [s.as_dict() for s in stations]
+
+
 def get_station_par(climate_pars: ClimatePars):
     stationManager = CligenStationsManager(climate_pars.database)
-    station = stationManager.get_station_fromid(climate_pars.par_id)
-    contents = open(station.parpath).read()
-    return Response(content=contents, media_type="application/text")
+    stationMeta = stationManager.get_station_fromid(climate_pars.par_id)
+    station = stationMeta.get_station()
+    
+    if climate_pars.location is not None and climate_pars.use_prism:
+        station = station.prism_mod(
+            climate_pars.location.longitude, 
+            climate_pars.location.latitude)
+        
+    return station
+    
+@router.post("/rockclim/GET/station_par")
+def get_station_par(climate_pars: ClimatePars):
+    station = get_station_par(climate_pars)
+    return Response(content=station.contents, media_type="application/text")
 
 
 @router.post("/rockclim/GET/station_par_monthlies")
 def get_station_par_monthlies(climate_pars: ClimatePars):
-    stationManager = CligenStationsManager(climate_pars.database)
-    station = stationManager.get_station_fromid(climate_pars.par_id)
-    return station.as_dict(include_monthlies=True)
+    station = get_station_par(climate_pars)
+    return station.get_monthlies()
+
 
 @router.post("/rockclim/GET/climate")
 def get_climate(climate_pars: ClimatePars):
@@ -77,15 +110,14 @@ def get_climate(climate_pars: ClimatePars):
         Response: A Response object containing the contents of the generated climate file 
                 with media type "application/text".
     """
-    stationManager = CligenStationsManager(climate_pars.database)
-    stationMeta = stationManager.get_station_fromid(climate_pars.par_id)
+    station = get_station_par(climate_pars)
     
     os.makedirs("/ramdisk/rockclim/", exist_ok=True)
 
     _hash = hash(climate_pars)
     cli_fname = f"{_hash}.cli"
     
-    cligen = Cligen(stationMeta, wd="/ramdisk/rockclim/", cliver=climate_pars.cligen_version)
+    cligen = Cligen(station, wd="/ramdisk/rockclim/", cliver=climate_pars.cligen_version)
     cligen.run_multiple_year(climate_pars.input_years, cli_fname=cli_fname)
     
     with open(f"/ramdisk/rockclim/{cli_fname}", "r") as file:
