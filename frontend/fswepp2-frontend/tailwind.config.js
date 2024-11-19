87f3b19318242906143e7df2b5086704405014b0
Damien Flutre <damien.flutre@wsu.edu>
2024-11-19 20:37:34 +0000

    Navigation bar layout

--- frontend/fswepp2-frontend/tailwind.config.js
+++ frontend/fswepp2-frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
