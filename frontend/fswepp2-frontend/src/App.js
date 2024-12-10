dd650157a205e9894a36f31c7905665c7a107fcb
Damien Flutre <144629417+DamiennFl@users.noreply.github.com>
2024-12-10 19:27:17 +0000

    Navbar mostly completed

--- frontend/fswepp2-frontend/src/App.js
+++ frontend/fswepp2-frontend/src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar/Navbar.jsx';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.jsx'; // Assuming you have a Navbar component
+import RockCliMe from './components/RockCliMe/RockCliMe.jsx';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/rockclime" element={<RockCliMe />} />
+        {/* Add other routes here */}
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/fswepp2-frontend/src/components/RockCliMe/RockCliMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RockCliMe = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default RockCliMe;
