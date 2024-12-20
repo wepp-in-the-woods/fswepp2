f8698147a74fed34e0020882ab660fb1841278d2
Damien Flutre <144629417+DamiennFl@users.noreply.github.com>
2024-12-20 19:45:31 +0000

    RockClime main page layout and flow

--- frontend/fswepp2-frontend/src/components/RockCliMe/RockCliMe.jsx
+++ frontend/fswepp2-frontend/src/components/RockCliMe/RockCliMe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from "react";
+import React, { useState, useEffect, memo, useRef } from "react";
 import {
   MapContainer,
   TileLayer,
@@ -13,6 +13,7 @@ import axios from "axios";
 import "./custom-scrollbar.css";
 import { useNavigate } from "react-router-dom";
 import ClimateData from "./ClimateData";
+import { latLng } from "leaflet";
 
 const LocationMarker = memo(
   ({ coordinates, setCoordinates, setLatInput, setLngInput }) => {
@@ -64,7 +65,14 @@ const RockCliMe = () => {
   const navigate = useNavigate();
   const [showSavedParameters, setShowSavedParameters] = useState(false);
   const [activeTab, setActiveTab] = useState("closestStations");
-  const [showTab, setShowTab] = useState(false);
+  const modalRef = useRef(null);
+  const [showLocationDiv, setShowLocationDiv] = useState(false);
+
+  useEffect(() => {
+    if (coordinates && !showLocationDiv && activeTab === "closestStations") {
+      handleGetClosestStations();
+    }
+  }, [coordinates, showLocationDiv, activeTab]);
 
   const handleCoordinateSubmit = () => {
     const lat = parseFloat(latInput);
@@ -96,7 +104,7 @@ const RockCliMe = () => {
   };
 
   const handleStationClick = (station) => {
-    setSelectedStation(station);
+    setSelectedStation(selectedStation === station ? null : station);
   };
 
   const handleYearsChange = (e) => {
@@ -156,62 +164,187 @@ const RockCliMe = () => {
         </div>
         <button
           onClick={() => navigate("/")}
-          className="px-4 py-2 bg-blue-500 text-white rounded"
+          className="px-4 py-2 bg-[#16a34a] text-white rounded"
         >
           Home
         </button>
       </div>
-      <div className="flex-grow z-0">
-        <MapContainer
-          center={coordinates || [39.8283, -98.5795]}
-          zoom={4}
-          scrollWheelZoom={true}
-          attributionControl={false}
-          style={{ zIndex: 0 }}
-          className="h-full w-full"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <LocationMarker
-            coordinates={coordinates}
-            setCoordinates={setCoordinates}
-            setLatInput={setLatInput}
-            setLngInput={setLngInput}
-          />
-          <MapUpdater coordinates={coordinates} />
-        </MapContainer>
-      </div>
-      <div className="flex flex-row mt-1 mb-1 justify-center w-full max-w-screen px-1">
-        <input
-          type="text"
-          placeholder="Latitude"
-          value={latInput}
-          onChange={(e) => setLatInput(e.target.value)}
-          className="flex-shrink px-2 py-1 border border-gray-300 rounded mr-2 w-1/3"
-        />
-        <input
-          type="text"
-          placeholder="Longitude"
-          value={lngInput}
-          onChange={(e) => setLngInput(e.target.value)}
-          className="flex-shrink px-2 py-1 border border-gray-300 rounded mr-2 w-1/3"
-        />
+      <div className="flex flex-row w-full pt-2 pb-2 justify-between items-center mt-2 mb-2 pl-2 pr-2">
+        <div className="text-sm md:text-base">
+          Current Location:{" "}
+          {latInput && lngInput
+            ? `${parseFloat(latInput).toFixed(3)}, ${parseFloat(
+                lngInput
+              ).toFixed(3)}`
+            : "None"}
+        </div>
         <button
-          onClick={handleCoordinateSubmit}
-          className="flex-shrink px-2 py-2 bg-blue-500 text-white rounded w-1/3 text-sm"
+          className="underline text-sm md:text-base"
+          onClick={() => setShowLocationDiv(true)}
         >
-          Set Coordinates
+          Choose Location
         </button>
       </div>
-      <div className="flex justify-center mt-2 w-full px-1 mb-2">
-        <button
-          onClick={handleGetClosestStations}
-          className="px-4 py-2 bg-green-500 text-white rounded text-lg w-full"
-        >
-          Get Closest Stations
-        </button>
+      {showLocationDiv && (
+        <div className="fixed inset-0 flex items-center justify-center z-0">
+          <div className="bg-white pl-4 pr-4 pb-4 rounded shadow-lg w-full ml-2 mr-2 border-2 h-2.3">
+            <button
+              className="mb-1 text-gray-500 flex flex-row w-full items-end text-xl rounded h-[30px] w-[30px]"
+              onClick={() => setShowLocationDiv(false)}
+            >
+              X
+            </button>
+            <div className="flex-grow z-0 h-60">
+              <MapContainer
+                center={coordinates || [39.8283, -98.5795]}
+                zoom={4}
+                scrollWheelZoom={true}
+                attributionControl={false}
+                style={{ zIndex: 0 }}
+                className="h-full w-full"
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <LocationMarker
+                  coordinates={coordinates}
+                  setCoordinates={setCoordinates}
+                  setLatInput={setLatInput}
+                  setLngInput={setLngInput}
+                  style={{ color: "green" }}
+                />
+                <MapUpdater coordinates={coordinates} />
+              </MapContainer>
+            </div>
+            <div className="mt-4">
+              <div className="flex flex-row justify-center w-full">
+                <input
+                  type="text"
+                  placeholder="Latitude"
+                  value={parseFloat(latInput).toFixed(5)}
+                  onChange={(e) =>
+                    setLatInput(parseFloat(e.target.value).toFixed(5))
+                  }
+                  className="flex-shrink px-2 py-1 border border-gray-300 rounded mr-2 w-1/3"
+                />
+                <input
+                  type="text"
+                  placeholder="Longitude"
+                  value={parseFloat(lngInput).toFixed(5)}
+                  onChange={(e) =>
+                    setLngInput(parseFloat(e.target.value).toFixed(5))
+                  }
+                  className="flex-shrink px-2 py-1 border border-gray-300 rounded mr-2 w-1/3"
+                />
+                <button
+                  onClick={() => { handleCoordinateSubmit(); setShowLocationDiv(false); }}
+                  className="flex-shrink px-2 py-2 bg-[#16a34a] text-white rounded w-1/3 text-sm"
+                >
+                  Set Coords.
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* Tabs */}
+      <div className="flex w-full border-t border-gray-300">
+        <div className="flex flex-row w-1/2 border-r-2">
+          <button
+            onClick={() => {
+              setActiveTab("closestStations");
+            }}
+            className={`px-4 py-2 w-full ${
+              activeTab === "closestStations"
+                ? "border-b border-green-700 bg-gray-100"
+                : "bg-white border-b border-white pb-2"
+            }`}
+          >
+            Closest Stations
+          </button>
+        </div>
+        <div className="flex flex-row w-1/2">
+          <button
+            onClick={() => {
+              setActiveTab("savedParameters");
+              setSelectedStation(null);
+            }}
+            className={`px-4 py-2 w-full ${
+              activeTab === "savedParameters"
+                ? "border-b border-green-700 bg-gray-100"
+                : "bg-white border-b border-white pb-2"
+            }`}
+          >
+            Saved Parameters
+          </button>
+        </div>
+      </div>
+      <div className="flex-grow overflow-auto p-4">
+        {activeTab === "closestStations" && (
+          <div className="grid grid-cols-1 gap-4">
+            {closestStations.slice(0, 6).map((station, index) => (
+              <div key={index}>
+                <button
+                  className={`border p-2 rounded text-left w-full ${
+                    selectedStation === station ? "bg-[#16a34a] text-white" : ""
+                  }`}
+                  onClick={() => handleStationClick(station)}
+                >
+                  <strong>{station.desc}</strong>
+                  <br />
+                  Latitude: {station.latitude}, Longitude: {station.longitude}
+                  <br />
+                  Distance: {station.distance_to_query_location.toFixed(2)} km
+                </button>
+                {selectedStation === station && (
+                  <div className="mt-2 p-2 border rounded bg-gray-100">
+                    <div className="mb-2">
+                      <label className="inline-flex items-center mb-2">
+                        <input
+                          type="checkbox"
+                          className="form-checkbox"
+                          checked={usePrism}
+                          onChange={(e) => setUsePrism(e.target.checked)}
+                        />
+                        <span className="ml-2">Use Prism monthlies</span>
+                      </label>
+                      <button
+                        className="block w-full text-left p-2 mb-2 bg-blue-500 text-white rounded"
+                        onClick={handleViewStationPar}
+                      >
+                        View Station Parameters
+                      </button>
+                    </div>
+                    <div className="mb-2 border-t-2 border-gray-300">
+                      <label className="mt-2 block text-sm font-medium text-gray-700">
+                        Number of Years
+                      </label>
+                      <input
+                        type="number"
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded"
+                        value={years}
+                        onChange={(e) => setYears(e.target.value)}
+                      />
+                    </div>
+                    <button
+                      className="block w-full text-left p-2 bg-green-500 text-white rounded"
+                      onClick={handleViewClimateData}
+                    >
+                      Generate Climate Data
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+        {activeTab === "savedParameters" && (
+          <div>
+            {/* Display saved parameters data here */}
+            <p>Saved Parameters content goes here.</p>
+          </div>
+        )}
       </div>
     </div>
   );
