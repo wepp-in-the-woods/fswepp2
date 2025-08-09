16de37dcb007f48c8d68869f5e3aea3a841378e1
Rohan Shrestha <sth.ronx@gmail.com>
2025-08-08 19:23:19 -0700

    Add additional unit conversions for peak flow.

--- frontend/src/hooks/use-units.ts
+++ frontend/src/hooks/use-units.ts
@@ -37,15 +37,29 @@ const useConversions = () => {
     kmToMiles: (km: number) => km * 0.621371,
     milesToKm: (miles: number) => miles * 1.60934,
 
-    // Precision conversions
-    mmToInches: (mm: number) => mm * 0.0393701,
+    // Length conversions
+    mmToInches: (mm: number) => mm / 25.4,
     inchesToMm: (inches: number) => inches * 25.4,
+    cmToInches: (cm: number) => cm / 2.54,
+    inchesToCm: (inches: number) => inches * 2.54,
+    mToFt: (m: number) => m * 3.281,
+    ftToM: (ft: number) => ft / 3.281,
 
     // Temperature conversions
     celsiusToFahrenheit: (celsius: number) => (celsius * 9/5) + 32,
     fahrenheitToCelsius: (fahrenheit: number) => (fahrenheit - 32) * 5/9,
 
+    // Area conversions
+    hectaresToAcres: (hectares: number) => hectares * 2.471,
+    acresToHectares: (acres: number) => acres / 2.471,
 
+    // Flow rate
+    cubicMetersPerSecToCubicFeetPerSec: (cms: number) => cms * 35.31,
+    cubicFeetPerSecToCubicMetersPerSec: (cfs: number) => cfs / 35.31,
+
+    // Unit peak flow rate (complex conversion)
+    quSItoUS: (qu: number) => qu * 35.31 / 2.471 / 25.4,
+    quUStoSI: (quUS: number) => quUS / (35.31 / 2.471 / 25.4)
   };
 
   return { convert };
