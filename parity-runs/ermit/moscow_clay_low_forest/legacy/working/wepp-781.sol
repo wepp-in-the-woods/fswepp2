ad9504ceb35c12ed15d88901453c7e8e0a02537b
Roger Lew <rogerlew@gmail.com>
2026-01-23 02:55:55 +0000

    Refresh ERMiT and Disturbed parity artifacts

--- parity-runs/ermit/moscow_clay_low_forest/legacy/working/wepp-781.sol
+++
@@ -1,7 +0,0 @@
-95.1
-#  WEPP 'clay loam' 'lll4' forest soil input file for ERMiT
-#  Data from U.S. Forest Service RMRS Air, Water and Aquatic Environments (AWAE) Project, Moscow FSL
-#  Created by 'createsoilfile' version 2014.02.06
-1	0
-'ERMiT_l4'	'clay loam'	1	0.2	0.75	500000	0.00025	4	8
-400	25	30	5	25	20
